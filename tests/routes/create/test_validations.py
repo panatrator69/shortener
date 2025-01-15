41900d632d207d8e8b817b87a0f8fde3ddbf9c7f
Anteater Proboscis <AnteaterProboscis@gmail.com>
2025-01-14 16:00:48 -0800

    tests: added validation test for http 400 response body.

--- tests/routes/create/test_validations.py
+++ tests/routes/create/test_validations.py
@@ -2,3 +2,4 @@ def test_empty_url(test_client):
     resp = test_client.post('/app/create', json={'url': ''})
 
     assert resp.status_code == 400
+    assert resp.json()['detail'] == 'URL cannot be empty'
